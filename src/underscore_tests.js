b202cdb8a985afab0b647553f602346db217d987
Joseph Blank <josephblank@Josephs-MacBook-Pro.local>
2016-09-19 17:09:54 -0600

    worked on first 2 problems

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,21 +16,37 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if (n) {
+      return array.slice(0, n);
+    } else {
+      return array[0];
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n) {
+      if (n < array.length) {
+      return array.slice(array.length - n, array.length);
+    } else {
+      return array;
+    }
+    } else {
+      return array[array.length -1];
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+      
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+    return array.indexOf(target);
   };
 
   // Return all elements of an array that pass a truth test ('iterator' function argument)
